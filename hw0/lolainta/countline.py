377e1da657d65154621cef8e720a35d5c1f8485d
LolaInTa Chi <lolainta@gmail.com>
2023-09-10 23:50:14 +0800

    style: format with black & add main function

--- hw0/lolainta/countline.py
+++ hw0/lolainta/countline.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
-
 import sys
 import os.path
 
 
-if len(sys.argv) < 2:
-    sys.stdout.write('missing file name\n')
-elif len(sys.argv) > 2:
-    sys.stdout.write('only one argument is allowed\n')
-else:
-    fname = sys.argv[1]
-    if os.path.exists(fname):
-        with open(fname) as fobj:
-            lines = fobj.readlines()
-        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+def main():
+    if len(sys.argv) < 2:
+        sys.stdout.write("missing file name\n")
+    elif len(sys.argv) > 2:
+        sys.stdout.write("only one argument is allowed\n")
     else:
-        sys.stdout.write('{} not found\n'.format(fname))
+        fname = sys.argv[1]
+        if os.path.exists(fname):
+            with open(fname) as fobj:
+                lines = fobj.readlines()
+            sys.stdout.write("{} lines in {}\n".format(len(lines), fname))
+        else:
+            sys.stdout.write("{} not found\n".format(fname))
+
+
+if __name__ == "__main__":
+    main()
