d9ecc19e2b3a5e201c1d2aea36d79c2e8fb074c8
LolaInTa Chi <lolainta@gmail.com>
2023-09-16 20:16:42 +0800

    feat: hw1 done

---
+++ hw1/lolainta/countline.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S ${PYTHON_BIN}g
+import sys
+import os.path
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.stdout.write("missing file name\n")
+    elif len(sys.argv) > 2:
+        sys.stdout.write("only one argument is allowed\n")
+    else:
+        fname = sys.argv[1]
+        if os.path.exists(fname):
+            with open(fname) as fobj:
+                lines = fobj.readlines()
+            sys.stdout.write("{} lines in {}\n".format(len(lines), fname))
+        else:
+            sys.stdout.write("{} not found\n".format(fname))
+
+
+if __name__ == "__main__":
+    main()
