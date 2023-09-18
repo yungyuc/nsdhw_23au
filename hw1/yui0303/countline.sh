70c48ec56e0519d4dfd8864bf9d23ecebdfec8ae
yuchang <iampeter62@gmail.com>
2023-09-18 21:25:59 +0800

    feat: add countline.py and countline.sh to hw1/yui0303

---
+++ hw1/yui0303/countline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+    echo "missing file name"
+elif [ $# -gt 1 ]; then
+        echo "only one argument is allowed"
+else 
+    if [ -e $1 ]; then
+        echo `cat $1 | wc -l` lines in $1
+    else
+        echo $1 "not found"
+    fi
+fi
