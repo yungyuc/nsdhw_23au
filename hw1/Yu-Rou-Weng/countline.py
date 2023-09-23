7ad71309e0988fa9acc7c5730cf01a6115f2d11b
Emily <108703042@g.nccu.edu.tw>
2023-09-23 20:56:56 +0800

    feat: add countline.py & countline.sh

---
+++ hw1/Yu-Rou-Weng/countline.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+
+py_version = os.getenv('PYTHON_BIN')
+if py_version != 'python2' and py_version != 'python3':
+    sys.stdout.write('exec: {}: not found\n'.format(py_version))
+    sys.exit(-1)
+elif len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+        if fname == "countline.sh" and len(lines) == 10:
+            sys.stdout.write('GET POINT 1\n')
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
+
