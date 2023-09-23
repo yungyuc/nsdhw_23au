efa059944a664c0f095917c1b107ece84c788e74
Lennart Holzenkamp (linux) <lennart.holzenkamp@rwth-aachen.de>
2023-09-24 01:10:12 +0800

    Adds validation of existence of the given file. Adds error descriptions to README.md

--- hw1/leholz/README.md
+++ hw1/leholz/README.md
@@ -1,1 +1,9 @@
 This directory is under development.
+
+# Exit Codes
+
+## 1
+The number of parameters is not exactly one.
+
+## 2 
+The file described by the first parameter does not exist.
--- hw1/leholz/countline.sh
+++ hw1/leholz/countline.sh
@@ -2,9 +2,16 @@
 
 int_params="$#"
 
+# Validating number of parameters.
 if ! [ "$int_params" = 1 ]; then
   echo "Please provide exactly one parameter."
-  exit 0
+  exit 1
+fi
+
+filename="$1"
+if ! [ -f "$filename" ]; then
+  echo "The given file $filename does not exists."
+  exit 2
 fi
 
 echo "Successfully terminated."
