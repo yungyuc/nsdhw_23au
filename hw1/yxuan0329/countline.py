a33150d0ac9bc726f5e20eb0e501fcf5908c641a
xuan <xuan94357@gmail.com>
2023-09-23 23:04:53 +0800

    feat: modify countline.py

--- hw1/yxuan0329/countline.py
+++ hw1/yxuan0329/countline.py
@@ -1,10 +1,14 @@
-#!/bin/bash
-
-"exec" "${PYTHON_BIN:-python3}" "$0" "$@"
+#!/usr/bin/env python
 
 import sys
 import os
 import os.path
+
+python_bin = os.popen('printenv PYTHON_BIN').read()
+python_bin = python_bin.strip()
+
+if os.system("{} --version > /dev/null 2>&1".format(python_bin)) != 0:
+    sys.exit("exec: {}: not found".format(python_bin))
 
 if len(sys.argv) < 2:
     sys.stdout.write('missing file name\n')
