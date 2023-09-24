3b3abde2e5ead23836d882f62275e5b531ba6bf0
yhlin <lin860707.cs10@nycu.edu.tw>
2023-09-24 19:43:30 +0800

    first commit

---
+++ hw1/pete0707/countline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+    echo "missing file name"
+elif [ "$#" -gt 1 ]; then
+    echo "only one argument is allowed"
+else
+    fname=$1
+    if [ -f $fname ]; then
+        line=$(wc -l < $fname)
+        echo "$line lines in $fname"
+    else
+        echo "$fname not found"
+    fi
+fi
