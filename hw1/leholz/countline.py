a493444e303e876bf03b016e304fcc5dae7b12aa
Lennart Holzenkamp (linux) <lennart.holzenkamp@rwth-aachen.de>
2023-09-24 13:44:27 +0800

    Adds reading of environment variable PYTHON_BIN_ENV. Ensures existence of this environment variable

---
+++ hw1/leholz/countline.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+
+PYTHON_BIN_ENV = 'PYTHON_BIN'
+python_bin_env_val = os.getenv(PYTHON_BIN_ENV)
+
+# Test ENV variable, return with error, if variable is not set.
+if python_bin_env_val is None:
+    print(f'Environment variable {PYTHON_BIN_ENV} is not set.')
+    quit(1)
+
+if len(sys.argv) < 2:
+    sys.stdout.write('missing file name\n')
+elif len(sys.argv) > 2:
+    sys.stdout.write('only one argument is allowed\n')
+else:
+    fname = sys.argv[1]
+    if os.path.exists(fname):
+        with open(fname) as fobj:
+            lines = fobj.readlines()
+        sys.stdout.write('{} lines in {}\n'.format(len(lines), fname))
+    else:
+        sys.stdout.write('{} not found\n'.format(fname))
