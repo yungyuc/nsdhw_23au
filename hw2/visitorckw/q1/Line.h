d800d50dfb0a8ed8d2714e156bb030b33591b118
Kuan-Wei Chiu <visitorckw@gmail.com>
2023-10-07 04:08:15 +0800

    feat: finish q1

---
+++ hw2/visitorckw/q1/Line.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include <cstdlib>
+
+class Line
+{
+public:
+    Line();
+    Line(Line const & );
+    Line(Line       &&);
+    Line & operator=(Line const & );
+    Line & operator=(Line       &&);
+    Line(size_t size);
+    ~Line();
+    size_t size() const;
+    float const & x(size_t it) const;
+    float & x(size_t it);
+    float const & y(size_t it) const;
+    float & y(size_t it);
+private:
+    std::vector<float> x_coords;
+    std::vector<float> y_coords;
+}; /* end class Line */
