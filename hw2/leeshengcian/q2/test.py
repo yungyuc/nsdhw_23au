99dbcfe75a4319a9d5ebad3a0016eeb8452be311
leeshengcian <sclee.cs12@nycu.edu.tw>
2023-10-06 20:21:27 +0800

    finish question 2

---
+++ hw2/leeshengcian/q2/test.py
@@ -0,0 +1,15 @@
+import pytest
+import math
+from angle_calculation import angle_py
+
+def test_zero_len():
+    assert math.isnan(angle_py((0, 0), (0, 0)))
+
+def test_zero_angle():
+    assert (angle_py((1, 1), (2, 2)) == 0)
+
+def test_right_angle():
+    assert (angle_py((1, 0), (0, 1)) == math.pi / 2)
+
+def test_any_angle():
+    assert (angle_py((1, 2), (3, 4)) == math.acos(11 / (math.sqrt(5) * math.sqrt(25))))
