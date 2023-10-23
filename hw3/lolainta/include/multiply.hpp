7bcc8c2feb09b59887ca59aee3de9ee1a3365cf9
LolaInTa Chi <lolainta@gmail.com>
2023-10-23 17:04:09 +0800

    test: dummy multiply without mkl tested

--- hw3/lolainta/include/multiply.hpp
+++ hw3/lolainta/include/multiply.hpp
@@ -4,5 +4,7 @@
 #include "Matrix.hpp"
 
 namespace Multiply {
-Matrix multiply_naive(const Matrix& A, const Matrix& B);
-}
+Matrix multiply_naive(const Matrix&, const Matrix&);
+Matrix multiply_tile(const Matrix&, const Matrix&, size_t);
+Matrix multiply_mkl(const Matrix&, const Matrix&);
+}  // namespace Multiply
--- hw3/lolainta/src/multiply.cpp
+++ hw3/lolainta/src/multiply.cpp
@@ -1,5 +1,9 @@
 #include "multiply.hpp"
 
+#include <mkl/mkl.h>
+#include <mkl/mkl_lapack.h>
+#include <mkl/mkl_lapacke.h>
+
 #include <stdexcept>
 
 Matrix Multiply::multiply_naive(const Matrix& A, const Matrix& B) {
@@ -14,5 +18,40 @@ Matrix Multiply::multiply_naive(const Matrix& A, const Matrix& B) {
       }
     }
   }
+  return C;
+}
+
+Matrix Multiply::multiply_tile(const Matrix& A, const Matrix& B,
+                               size_t tile_size) {
+  if (A.get_cols() != B.get_rows()) {
+    throw std::invalid_argument("Matrix dimensions do not match");
+  }
+  Matrix C(A.get_rows(), B.get_cols());
+  for (size_t i = 0; i < A.get_rows(); i += tile_size) {
+    for (size_t j = 0; j < B.get_cols(); j += tile_size) {
+      for (size_t k = 0; k < A.get_cols(); k += tile_size) {
+        for (size_t ii = i; ii < std::min(i + tile_size, A.get_rows()); ii++) {
+          for (size_t jj = j; jj < std::min(j + tile_size, B.get_cols());
+               jj++) {
+            for (size_t kk = k; kk < std::min(k + tile_size, A.get_cols());
+                 kk++) {
+              C(ii, jj) += A(ii, kk) * B(kk, jj);
+            }
+          }
+        }
+      }
+    }
+  }
+  return C;
+}
+
+Matrix Multiply::multiply_mkl(const Matrix& A, const Matrix& B) {
+  if (A.get_cols() != B.get_rows()) {
+    throw std::invalid_argument("Matrix dimensions do not match");
+  }
+  Matrix C(A.get_rows(), B.get_cols());
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, A.get_rows(),
+              B.get_cols(), A.get_cols(), 1, A.data[0], A.get_cols(), B.data[0],
+              B.get_cols(), 0, C.data[0], C.get_cols());
   return C;
 }
