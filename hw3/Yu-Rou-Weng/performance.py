0f863b1d6968ce572c2411e90b7e123e1950549b
Yu-Rou-Weng <108703042@nccu.edu.tw>
2023-11-03 02:58:14 +0800

    Submit Hw3

---
+++ hw3/Yu-Rou-Weng/performance.py
@@ -0,0 +1,58 @@
+import matrix
+import sys
+import timeit
+
+class Writer:
+
+    def __init__(self, streams):
+
+        self.streams = streams
+
+    def write(self, msg):
+
+        for stream in self.streams:
+
+            stream.write(msg)
+
+def make_matrices(size):
+    mat1 = matrix.Matrix(size, size)
+    mat2 = matrix.Matrix(size, size)
+    mat3 = matrix.Matrix(size, size)
+
+    for it in range(size):
+        for jt in range(size):
+            mat1[it, jt] = it * size + jt 
+            mat2[it, jt] = it * size + jt
+            mat3[it, jt] = 0
+    return mat1, mat2, mat3
+
+def benchmark():
+    tsize = 16 
+    mat1, mat2, mat3 = make_matrices(1000)
+    ns = dict(matrix=matrix, mat1=mat1, mat2=mat2, tsize=tsize)
+    t_naive = timeit.Timer('matrix.multiply_naive(mat1, mat2)', globals=ns)
+    t_tile = timeit.Timer("matrix.multiply_tile(mat1, mat2, tsize)", globals=ns)
+    t_mkl = timeit.Timer("matrix.multiply_mkl(mat1, mat2)", globals=ns)
+
+    with open('performance.txt', 'w') as fobj:
+        repeat = 5
+
+        w = Writer([sys.stdout, fobj])
+        w.write('Start multiply_naive using stride=16 (repeat={}), take min = '.format(repeat))
+        naivesec = minsec = min(t_naive.repeat(repeat=repeat, number=1))
+        w.write('{} seconds\n'.format(minsec))
+
+        w.write('Start multiply_tile (repeat={}), take min = '.format(repeat))
+        tilesec = minsec = min(t_tile.repeat(repeat=repeat, number=1))
+        w.write('{} seconds\n'.format(minsec))
+
+        w.write('Start multiply_mkl (repeat={}), take min = '.format(repeat))
+        mklsec = minsec = min(t_mkl.repeat(repeat=repeat, number=1))
+        w.write('{} seconds\n'.format(minsec))
+
+
+        w.write('Tile speed-up over naive: %g x\n' % (naivesec/tilesec))
+        w.write('MKL speed-up over naive: %g x\n' % (naivesec/mklsec))
+
+if __name__ == '__main__':
+    benchmark()
---
+++ hw3/Yu-Rou-Weng/test_performance.py
@@ -0,0 +1,68 @@
+import matrix
+import pytest
+import timeit
+
+class Test_Matrix:
+    @staticmethod
+    def matrices(dimension):
+        matrix_a = matrix.Matrix(dimension, dimension)
+        matrix_b = matrix.Matrix(dimension, dimension)
+
+        for i in range(dimension):
+            for j in range(dimension):
+                matrix_a[i, j] = i * dimension + j + 1
+                matrix_b[i, j] = i * dimension + j + 1
+
+        return matrix_a, matrix_b
+
+    def test_MatrixMultiply_correct(self):
+        dim = 100
+        m1, m2 = self.matrices(dim)
+
+        result_naive = matrix.multiply_naive(m1, m2)
+        result_mkl = matrix.multiply_mkl(m1, m2)
+
+        for i in range(dim):
+            for j in range(dim):
+                assert result_naive[i, j] == result_mkl[i, j]
+
+    def test_MatrixMultiply_zero(self):
+        dim = 100
+        m1, m2 = self.matrices(dim)
+        m_zero = matrix.Matrix(dim, dim)  
+        for i in range(dim):
+            for j in range(dim):
+                m_zero[i, j] = 0
+
+        result_naive = matrix.multiply_naive(m1, m_zero)
+        result_mkl = matrix.multiply_mkl(m1, m_zero)
+
+        for i in range(dim):
+            for j in range(dim):
+                assert result_naive[i, j] == 0
+                assert result_mkl[i, j] == 0
+
+    def test_tiling_efficiency(self):
+        matrix_size = 1000
+        m1, m2 = self.matrices(matrix_size)
+        base_tile_size = 0 
+        ratio_base= self.tiling_test(m1, m2, base_tile_size)
+
+        for tile_size in [16, 17, 19]:
+            ratio= self.tiling_test(m1, m2, tile_size)
+            assert ratio < 0.8 * ratio_base, f"Tiling with size {tile_size} should be at least 20% faster."
+
+    @staticmethod
+    def tiling_test(mat1, mat2, tile_size):
+        def timed_execution(code, globals):
+            return min(timeit.Timer(code, globals=globals).repeat(10, 1))
+
+        globals_for_testing = dict(matrix=matrix, mat1=mat1, mat2=mat2, tile_size=tile_size)
+        tile_code = f"matrix.multiply_tile(mat1, mat2, tile_size)" if tile_size else "matrix.multiply_naive(mat1, mat2)"
+
+        time_tiled = timed_execution(tile_code, globals_for_testing)
+        time_mkl = timed_execution('matrix.multiply_mkl(mat1, mat2)', globals_for_testing)
+
+        performance_ratio = time_tiled / time_mkl
+
+        return performance_ratio, time_tiled
