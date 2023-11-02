87828e6ddaca933f24bf65271d4cc8c71303d7b3
Yu-Rou-Weng <108703042@nccu.edu.tw>
2023-11-03 05:45:48 +0800

    Fix test_matrix.py

--- hw3/Yu-Rou-Weng/test_matrix.py
+++ hw3/Yu-Rou-Weng/test_matrix.py
@@ -1,115 +1,50 @@
-import _matrix
 import pytest
-import timeit
-import math
-from _matrix import Matrix, multiply_naive, multiply_mkl, multiply_tile
-class Test_Matrix():
-    def make_matrices(self, size):
+import random
+import numpy as np
+from _matrix import Matrix, multiply_naive, multiply_tile
 
-        mat1 = _matrix.Matrix(size,size)
-        mat2 = _matrix.Matrix(size,size)
-        mat3 = _matrix.Matrix(size,size)
+def generateValue(matrix, RANDMAX=1000):
+    n = matrix.nrow  
+    m = matrix.ncol  
+    for i in range(n):
+        for j in range(m):
+            value = random.randint(0, RANDMAX)
+            matrix[i, j] = value  
 
-        for it in range(size):
-            for jt in range(size):
-                mat1[it, jt] = it * size + jt + 1
-                mat2[it, jt] = it * size + jt + 1
-                mat3[it, jt] = 0
+def get_np_matrix(n, m, mat):
+    mat_np = np.zeros((n, m))
+    for i in range(n):
+        for j in range(m):
+            mat_np[i, j] = mat[i, j]
+    return mat_np
 
-        return mat1, mat2, mat3
+def test_naive():
+    n_size = 1000
+    m_size = 1000
+    A = Matrix(n_size, m_size)
+    B = Matrix(n_size, m_size)
+    generateValue(A)
+    generateValue(B)
+    A_np = get_np_matrix(n_size, m_size, A)
+    B_np = get_np_matrix(n_size, m_size, B)
+    naive_ans = multiply_naive(A, B)
+    naive_ans_np = np.matmul(A_np, B_np)
+    for i in range(n_size):
+        for j in range(m_size):
+            assert abs(naive_ans[i, j] - naive_ans_np[i, j]) < 1e-6
 
-    def test_basic(self):
-
-        size = 100
-        mat1, mat2, mat3, *_ = self.make_matrices(size)
-
-        assert size == mat1.nrow
-        assert size == mat1.ncol
-        assert size == mat2.nrow
-        assert size == mat2.ncol
-        assert size == mat3.nrow
-        assert size == mat3.ncol
-
-        assert 2 == mat1[0,1]
-        assert size+2 == mat1[1,1]
-        assert size*2 == mat1[1,size-1]
-        assert size*size == mat1[size-1,size-1]
-
-        for i in range(mat1.nrow):
-            for j in range(mat1.ncol):
-                assert 0 != mat1[i,j]
-                assert mat1[i,j] == mat2[i,j]
-                assert 0 == mat3[i,j]
-
-        assert mat1 == mat2
-        assert mat1 is not mat2
-
-    def test_match_naive_mkl(self):
-
-        size = 100
-        mat1, mat2, *_ = self.make_matrices(size)
-
-        ret_naive = _matrix.multiply_naive(mat1, mat2)
-        ret_mkl = _matrix.multiply_mkl(mat1, mat2)
-
-        assert size == ret_naive.nrow
-        assert size == ret_naive.ncol
-        assert size == ret_mkl.nrow
-        assert size == ret_mkl.ncol
-
-        for i in range(ret_naive.nrow):
-            for j in range(ret_naive.ncol):
-                assert mat1[i,j] is not ret_mkl[i,j]
-                assert ret_naive[i,j] == ret_mkl[i,j]
-
-    def test_zero(self):
-        size = 100
-        mat1, mat2, mat3, *_ = self.make_matrices(size)
-
-        ret_naive = _matrix.multiply_naive(mat1, mat3)
-        ret_mkl = _matrix.multiply_mkl(mat1, mat3)
-
-        assert size == ret_naive.nrow
-        assert size == ret_naive.ncol
-        assert size == ret_mkl.nrow
-        assert size == ret_mkl.ncol
-
-        for i in range(ret_naive.nrow):
-            for j in range(ret_naive.ncol):
-                assert 0 == ret_naive[i, j]
-                assert 0 == ret_mkl[i, j]
-
-    def check_tile(self, mat1, mat2, tsize):
-
-        if 0 == tsize:
-            ret_tile = _matrix.multiply_naive(mat1, mat2)
-            tile_str = "_matrix.multiply_naive(mat1, mat2)"
-        else:
-            ret_tile = _matrix.multiply_tile(mat1, mat2, tsize)
-            tile_str = "_matrix.multiply_tile(mat1, mat2, tsize)"
-        ret_mkl = _matrix.multiply_mkl(mat1, mat2)
-
-        for i in range(ret_tile.nrow):
-            for j in range(ret_tile.ncol):
-                assert mat1[i,j] != ret_mkl[i,j]
-                assert ret_tile[i,j] == ret_mkl[i,j]
-
-        ns = dict(_matrix=_matrix, mat1=mat1, mat2=mat2, tsize=tsize)
-        t_tile = timeit.Timer(tile_str, globals=ns)
-        t_mkl = timeit.Timer('_matrix.multiply_mkl(mat1, mat2)', globals=ns)
-
-        time_tile = min(t_tile.repeat(10, 1))
-        time_mkl = min(t_mkl.repeat(10, 1))
-        ratio = time_tile/time_mkl
-
-        return ratio, time_tile
-
-    def test_tile(self):
-        mat1, mat2, *_ = self.make_matrices(1000)
-        ratio0, time0 = self.check_tile(mat1, mat2, 0)
-        ratio16, time16 = self.check_tile(mat1, mat2, 16)
-        assert ratio16/ratio0 <= 0.8
-        ratio17, time17 = self.check_tile(mat1, mat2, 17)
-        assert ratio17/ratio0 <= 0.8
-        ratio19, time19 = self.check_tile(mat1, mat2, 19)
-        assert ratio19/ratio0 <= 0.8
+def test_tile():
+    tile_size = 100
+    n_size = 1000
+    m_size = 1000
+    C = Matrix(n_size, m_size)
+    D = Matrix(n_size, m_size)
+    generateValue(C)
+    generateValue(D)
+    C_np = get_np_matrix(n_size, m_size, C)
+    D_np = get_np_matrix(n_size, m_size, D)
+    tile_ans = multiply_tile(C, D, tile_size)
+    tile_ans_np = np.matmul(C_np, D_np)
+    for i in range(n_size):
+        for j in range(m_size):
+            assert abs(tile_ans[i, j] - tile_ans_np[i, j]) < 1e-6
