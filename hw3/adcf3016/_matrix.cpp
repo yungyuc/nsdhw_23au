9037b50b8367b78783c0a3f5f9a07879c48a3011
adcf3016 <adcf3016@gmail.com>
2023-11-03 11:27:55 +0800

    finish makefile pytest _matrix

---
+++ hw3/adcf3016/_matrix.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+#include <mkl.h>
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+class Matrix {
+   public:
+    Matrix(int row, int col) : m_row(row), m_col(col), m_key(new double[row * col]) {
+        memset(m_key, 0, sizeof(double) * row * col);
+    }
+
+    int nrow() const {
+        return m_row;
+    }
+
+    int ncol() const {
+        return m_col;
+    }
+
+    double& operator()(int x, int y) {
+        return m_key[x * m_col + y];
+    }
+
+    double operator()(int x, int y) const {
+        return m_key[x * m_col + y];
+    }
+
+    bool operator==(const Matrix& mat) const {
+        if (mat.m_col != this->m_col || mat.m_row == this->m_row) {
+            return false;
+        }
+        for (int i = 0; i < this->m_row; i++) {
+            for (int j = 0; j < this->m_col; j++) {
+                if ((*this)(i, j) != mat(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    double* get_key() const {
+        return m_key;
+    }
+
+   private:
+    int m_row;
+    int m_col;
+    double* m_key;
+};
+
+Matrix multiply_naive(Matrix const& mat1, Matrix const& mat2) {
+    assert(mat1.ncol() == mat2.nrow());
+
+    Matrix output(mat1.nrow(), mat2.ncol());
+    for (int i = 0; i < mat1.nrow(); i++) {
+        for (int j = 0; j < mat2.ncol(); j++) {
+            double sum = 0;
+            for (int k = 0; k < mat1.ncol(); k++) {
+                sum += mat1(i, k) * mat2(k, j);
+            }
+            output(i, j) = sum;
+        }
+    }
+    return output;
+}
+
+Matrix multiply_tile(Matrix const& mat1, Matrix const& mat2, int tile_size) {
+    assert(mat1.ncol() == mat2.nrow());
+
+    Matrix output(mat1.nrow(), mat2.ncol());
+    for (int i = 0; i < mat1.nrow(); i += tile_size) {
+        for (int j = 0; j < mat2.ncol(); j += tile_size) {
+            for (int k = 0; k < mat1.ncol(); k += tile_size) {
+                for (int ii = i; ii < std::min(i + tile_size, mat1.nrow()); ++ii) {
+                    for (int jj = j; jj < std::min(j + tile_size, mat2.ncol()); ++jj) {
+                        double sum = 0.0;
+                        for (int kk = k; kk < std::min(k + tile_size, mat1.ncol()); ++kk) {
+                            sum += mat1(ii, kk) * mat2(kk, jj);
+                        }
+                        output(ii, jj) += sum;
+                    }
+                }
+            }
+        }
+    }
+
+    return output;
+}
+
+Matrix multiply_mkl(Matrix const& mat1, Matrix const& mat2) {
+    assert(mat1.ncol() == mat2.nrow());
+
+    Matrix output(mat1.nrow(), mat2.ncol());
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, mat1.nrow(), mat1.ncol(), mat2.ncol(), 1, mat1.get_key(), mat1.ncol(), mat2.get_key(), mat2.ncol(), 0, output.get_key(), mat2.ncol());
+    return output;
+}
+
+PYBIND11_MODULE(_matrix, m) {
+    m.def("multiply_naive", &multiply_naive);
+    m.def("multiply_tile", &multiply_tile);
+    m.def("multiply_mkl", &multiply_mkl);
+    py::class_<Matrix>(m, "Matrix")
+        .def(py::init<int, int>())
+        .def_property_readonly("nrow", [](const Matrix& mat) { return mat.nrow(); })
+        .def_property_readonly("ncol", [](const Matrix& mat) { return mat.ncol(); })
+        .def("__eq__", [](const Matrix& a, const Matrix& b) { return a == b; })
+        .def("__setitem__", [](Matrix& self, std::pair<int, int> idx, double val) {
+            self(idx.first, idx.second) = val;
+        })
+        .def("__getitem__", [](const Matrix& self, std::pair<int, int> idx) {
+            return self(idx.first, idx.second);
+        });
+}
---
+++ hw3/adcf3016/test_matrix.py
@@ -0,0 +1,61 @@
+import pytest
+import numpy as np
+from _matrix import *
+
+def get_np_matrix(n, m, mat):
+    mat_np = np.zeros((n, m))
+    for i in range(n):
+        for j in range(m):
+            mat_np[i, j] = mat[i, j]
+    return mat_np
+
+def generate_value(mat):
+    for i in range(mat.nrow):
+        for j in range(mat.ncol):
+            mat[i, j] = i * mat.nrow + j
+
+def test_naive():
+    row_size = 1000
+    col_size = 1000
+    mat1 = Matrix(row_size, col_size)
+    mat2 = Matrix(row_size, col_size)
+    generate_value(mat1)
+    generate_value(mat2)
+    A_np = get_np_matrix(row_size, col_size, mat1)
+    B_np = get_np_matrix(row_size, col_size, mat2)
+    naive_ans = multiply_naive(mat1, mat2)
+    naive_ans_np = np.matmul(A_np, B_np)
+    for i in range(row_size):
+        for j in range(col_size):
+            assert abs(naive_ans[i, j] - naive_ans_np[i, j]) < 1e-6
+
+def test_tile():
+    tile_size = 16
+    row_size = 1000
+    col_size = 1000
+    mat1 = Matrix(row_size, col_size)
+    mat2 = Matrix(row_size, col_size)
+    generate_value(mat1)
+    generate_value(mat2)
+    C_np = get_np_matrix(row_size, col_size, mat1)
+    D_np = get_np_matrix(row_size, col_size, mat2)
+    tile_ans = multiply_tile(mat1, mat2, tile_size)
+    tile_ans_np = np.matmul(C_np, D_np)
+    for i in range(row_size):
+        for j in range(col_size):
+            assert abs(tile_ans[i, j] - tile_ans_np[i, j]) < 1e-6
+
+def test_mkl():
+    row_size = 1000
+    col_size = 1000
+    mat1 = Matrix(row_size, col_size)
+    mat2 = Matrix(row_size, col_size)
+    generate_value(mat1)
+    generate_value(mat2)
+    A_np = get_np_matrix(row_size, col_size, mat1)
+    B_np = get_np_matrix(row_size, col_size, mat2)
+    mkl_ans = multiply_mkl(mat1, mat2)
+    mkl_ans_np = np.matmul(A_np, B_np)
+    for i in range(row_size):
+        for j in range(col_size):
+            assert abs(mkl_ans[i, j] - mkl_ans_np[i, j]) < 1e-6
