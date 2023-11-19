4723679421593842880872101314a24d5bafcc2e
threemonth03 <austin20463@gmail.com>
2023-11-19 20:37:18 +0000

    threemonth hw4 attempt2

--- hw4/ThreeMonth03/_matrix.cpp
+++ hw4/ThreeMonth03/_matrix.cpp
@@ -1,51 +1,5 @@
 #include "_matrix.hpp"
 
-Matrix::Matrix(size_t row, size_t col) : m_nrow(row), m_ncol(col), m_buffer(MyAllocator){
-    m_buffer.resize(row * col);
-}
-
-Matrix::Matrix(Matrix const &mat) : m_nrow(mat.nrow()), m_ncol(mat.ncol()), m_buffer(mat.m_buffer, MyAllocator){
-
-}
-
-double Matrix::operator()(size_t row, size_t col) const{ 
-    return m_buffer[row*m_ncol + col];
-}
-
-double& Matrix::operator()(size_t row, size_t col){ 
-    return m_buffer[row*m_ncol + col];
-}
-
-double* Matrix::data(){ 
-    return m_buffer.data();
-}
-
-double const* Matrix::data() const{ 
-    return m_buffer.data();
-}
-
-size_t Matrix::nrow() const{ 
-    return m_nrow;
-}
-
-size_t Matrix::ncol() const{ 
-    return m_ncol; 
-}
-
-bool Matrix::operator==(Matrix const &mat2) const{
-    if (m_ncol != mat2.ncol() || m_nrow != mat2.ncol())
-        return false;
-    for (size_t i = 0; i < m_nrow; i++)
-    {
-        for (size_t j = 0; j < m_ncol; j++)
-        {
-            if (m_buffer[i * m_ncol + j] != mat2(i, j))
-                return false;
-        }
-    }
-    return true;
-}
-
 Matrix multiply_naive(Matrix const& mat1, Matrix const& mat2){
     Matrix result(mat1.nrow(), mat2.ncol());
 
@@ -53,7 +7,7 @@ Matrix multiply_naive(Matrix const& mat1, Matrix const& mat2){
     {
         for (size_t j = 0; j < mat2.ncol(); ++j)
         {
-	    double sum = 0.0;
+	    double sum = .0;
             for (size_t k = 0; k < mat1.ncol(); ++k)
             {
                 sum += mat1(i, k) * mat2(k, j);
@@ -66,8 +20,8 @@ Matrix multiply_naive(Matrix const& mat1, Matrix const& mat2){
 }
 
 Matrix multiply_tile(Matrix const& mat1, Matrix const& mat2, size_t tsize){
-    Matrix result(mat1.nrow(), mat2.ncol());
 
+    Matrix result(mat1.nrow(), mat2.ncol());
     size_t m = mat1.nrow(), n = mat2.ncol(), p = mat1.ncol();
 
     for (size_t i = 0; i < m; i += tsize)
@@ -80,7 +34,7 @@ Matrix multiply_tile(Matrix const& mat1, Matrix const& mat2, size_t tsize){
                 {
                     for (size_t jj = j; jj < std::min(j + tsize, n); ++jj) 
 		            {
-		    	        double sum = 0.0;
+		    	        double sum = .0;
                         for (size_t kk = k; kk < std::min(k + tsize, p); ++kk)
                         {
                             sum += mat1(ii, kk) * mat2(kk, jj);
@@ -96,8 +50,8 @@ Matrix multiply_tile(Matrix const& mat1, Matrix const& mat2, size_t tsize){
 }
 
 Matrix multiply_mkl(Matrix const& mat1, Matrix const& mat2){
-    Matrix result(mat1.nrow(), mat2.ncol());
 
+    Matrix result(mat1.nrow(), mat2.ncol());
     cblas_dgemm(
         CblasRowMajor, 
         CblasNoTrans, 
@@ -113,7 +67,7 @@ Matrix multiply_mkl(Matrix const& mat1, Matrix const& mat2){
         0.0, 
         result.data(), 
         result.ncol()
-    );
+        );
 
     return result;
 }
--- hw4/ThreeMonth03/_matrix.hpp
+++ hw4/ThreeMonth03/_matrix.hpp
@@ -1,23 +1,48 @@
 #ifndef _MATRIX_HPP_
 #define _MATRIX_HPP_
 
-#include <vector>
-#include <mkl.h>
 #include "_allocator.hpp"
+#include <vector>
+#include <utility>
+#include <mkl.h>
 
 static CustomAllocator<double> MyAllocator;
+inline size_t bytes() { return MyAllocator.counter.bytes(); }
+inline size_t allocated() { return MyAllocator.counter.allocated(); }
+inline size_t deallocated() { return MyAllocator.counter.deallocated(); }
 
-class Matrix{
+class Matrix
+{
+
 public:
-    Matrix(size_t row, size_t col);
-    Matrix(Matrix const &mat);
-    double operator()(size_t row, size_t col) const;
-    double &operator()(size_t row, size_t col);
-    bool operator==(Matrix const &mat2) const;
-    double *data();
-    double const *data() const;
-    size_t nrow() const;
-    size_t ncol() const;
+    Matrix(size_t row, size_t col) : m_nrow(row), m_ncol(col), m_buffer(MyAllocator) {
+        m_buffer.resize(row * col);
+    }
+
+    Matrix(Matrix const &mat) : m_nrow(mat.nrow()), m_ncol(mat.ncol()), m_buffer(mat.m_buffer, MyAllocator) {}
+    bool operator==(Matrix const &mat2) const
+    {
+        if (m_ncol != mat2.ncol() || m_nrow != mat2.ncol())
+            return false;
+
+        for (size_t i = 0; i < m_nrow; i++)
+        {
+            for (size_t j = 0; j < m_ncol; j++)
+            {
+                if (m_buffer[i * m_ncol + j] != mat2(i, j))
+                    return false;
+            }
+        }
+        return true;
+    }
+    double operator()(size_t row, size_t col) const { return m_buffer[row*m_ncol + col]; }
+    double &operator()(size_t row, size_t col) { return m_buffer[row*m_ncol + col]; }
+
+    double *data() { return m_buffer.data(); }
+    double const *data() const { return m_buffer.data(); }
+
+    size_t nrow() const { return m_nrow; }
+    size_t ncol() const { return m_ncol; }
 
 private:
     size_t m_nrow;
@@ -25,15 +50,6 @@ private:
     std::vector<double, CustomAllocator<double>> m_buffer;
 };
 
-inline size_t bytes(){ 
-    return MyAllocator.counter.bytes(); 
-}
-inline size_t allocated(){ 
-    return MyAllocator.counter.allocated();
-}
-inline size_t deallocated(){
-    return MyAllocator.counter.deallocated();
-}
 
 Matrix multiply_tile(Matrix const& mat1, Matrix const& mat2, size_t tsize);
 Matrix multiply_naive(Matrix const& mat1, Matrix const& mat2);
