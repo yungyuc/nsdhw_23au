bdb16b4c30e1485e0cea83305c051e4efb42bf71
adcf3016 <adcf3016@gmail.com>
2023-11-29 21:15:55 +0800

    Finish matrix.hpp

---
+++ hw5/adcf3016/matrix.hpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <mkl/mkl.h>
+
+class Matrix {
+   public:
+    Matrix(int row, int col) : m_row(row), m_col(col), m_key(new double[row * col]) {
+        memset(m_key, 0, sizeof(double) * row * col);
+    }
+
+    int nrow() const {
+        return m_row;
+    }
+
+    int ncol() const {
+        return m_col;
+    }
+
+    double& operator()(int x, int y) {
+        return m_key[x * m_col + y];
+    }
+
+    double operator()(int x, int y) const {
+        return m_key[x * m_col + y];
+    }
+
+    bool operator==(const Matrix& mat) const {
+        if (mat.m_col != this->m_col || mat.m_row != this->m_row) {
+            return false;
+        }
+        for (int i = 0; i < this->m_row; i++) {
+            for (int j = 0; j < this->m_col; j++) {
+                if ((*this)(i, j) != mat(i, j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    double* get_key() const {
+        return m_key;
+    }
+
+   private:
+    int m_row;
+    int m_col;
+    double* m_key;
+};
+
+Matrix multiply_naive(Matrix const& mat1, Matrix const& mat2) {
+    assert(mat1.ncol() == mat2.nrow());
+
+    Matrix output(mat1.nrow(), mat2.ncol());
+    for (int i = 0; i < mat1.nrow(); i++) {
+        for (int j = 0; j < mat2.ncol(); j++) {
+            double sum = 0;
+            for (int k = 0; k < mat1.ncol(); k++) {
+                sum += mat1(i, k) * mat2(k, j);
+            }
+            output(i, j) = sum;
+        }
+    }
+    return output;
+}
+
+Matrix multiply_tile(Matrix const& mat1, Matrix const& mat2, int tile_size) {
+    assert(mat1.ncol() == mat2.nrow());
+
+    Matrix output(mat1.nrow(), mat2.ncol());
+    for (int i = 0; i < mat1.nrow(); i += tile_size) {
+        for (int j = 0; j < mat2.ncol(); j += tile_size) {
+            for (int k = 0; k < mat1.ncol(); k += tile_size) {
+                for (int ii = i; ii < std::min(i + tile_size, mat1.nrow()); ii++) {
+                    for (int jj = j; jj < std::min(j + tile_size, mat2.ncol()); jj++) {
+                        for (int kk = k; kk < std::min(k + tile_size, mat1.ncol()); kk++) {
+                            output(ii, jj) += mat1(ii, kk) * mat2(kk, jj);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return output;
+}
+
+Matrix multiply_mkl(Matrix const& mat1, Matrix const& mat2) {
+    assert(mat1.ncol() == mat2.nrow());
+
+    Matrix output(mat1.nrow(), mat2.ncol());
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, mat1.nrow(), mat1.ncol(), mat2.ncol(), 1, mat1.get_key(), mat1.ncol(), mat2.get_key(), mat2.ncol(), 0, output.get_key(), mat2.ncol());
+    return output;
+}
